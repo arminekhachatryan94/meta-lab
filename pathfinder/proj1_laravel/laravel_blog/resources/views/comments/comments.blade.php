db5924706f81ba83668cc04320da984087bcbe42
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-03-27 15:39:48 -0700

    made comments a scroll box

--- pathfinder/proj1_laravel/laravel_blog/resources/views/comments/comments.blade.php
+++ pathfinder/proj1_laravel/laravel_blog/resources/views/comments/comments.blade.php
@@ -3,11 +3,13 @@
     <b>Comments</b>
 </div>
 
+<div style="height:200px; width:100%; overflow:scroll; overflow-y: scroll;">
 @foreach ($post->comments as $comment)
 
     @include ('comments.comment')
 
 @endforeach
+</div>
 
 <div>
     @include ('comments.create')
