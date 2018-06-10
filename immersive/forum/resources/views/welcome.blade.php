397b09f559ec36a02bc8b6920adc94a318e35cd8
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-06-09 20:15:26 -0700

    changed users table and updated registration

--- immersive/forum/resources/views/welcome.blade.php
+++ immersive/forum/resources/views/welcome.blade.php
@@ -89,6 +89,8 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+
+                <img src="{{ asset('img/dolphin.jpg') }}" alt="dolphin" height="200">
             </div>
         </div>
     </body>
